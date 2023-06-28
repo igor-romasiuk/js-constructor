a2d716b89ef0be68f3bf1e443f13beaf01d3272d
Igor <igor132143@gmail.com>
2023-06-28 16:54:43 +0300

    Added initial project setup

---
+++ src/model.js
@@ -0,0 +1,10 @@
+export const model = [
+    {type: 'title', value: 'hello world'},
+    {type: 'text', value: 'here text'},
+    {type: 'columns', value: [
+        '111111111111',
+        '121112112111111',
+        '1311113311111131'
+    ]},
+    {type: 'image', value: './assets/image.png'}
+]
