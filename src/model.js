a7b4f3450630252c3fee456b26a311e89133f3c7
Igor <igor132143@gmail.com>
2023-06-30 16:03:06 +0300

    add changes to the js-constructor

--- src/model.js
+++ src/model.js
@@ -1,10 +1,20 @@
+import image from './assets/image.png'
+
 export const model = [
-    {type: 'title', value: 'hello world'},
+    {type: 'title', value: 'Конструктор сайтів на JavaScript', options: {
+        tag: 'h2',
+        styles: {
+            background: 'liner-gradient(to right, #ff0099, #493240)',
+            color: '#fff',
+            padding: '1.5rem',
+            'text-align': 'center'
+        }
+    }},
     {type: 'text', value: 'here text'},
     {type: 'columns', value: [
         '111111111111',
         '121112112111111',
         '1311113311111131'
     ]},
-    {type: 'image', value: './assets/image.png'}
+    {type: 'image', value: image}
 ]
